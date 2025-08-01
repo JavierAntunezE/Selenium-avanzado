cbed912ab0e0af9f20e4d475c5ecbe96dc15c27f
javier-rrkd\student <javier.ant.4@gmail.com>
2025-08-01 15:41:03 -0400

    Se agrega evidencia automatizada en TestCarrito

--- src/test/java/com/selenium/distribuido/CarritoComprasSauce.java
+++ src/test/java/com/selenium/distribuido/CarritoComprasSauce.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -32,42 +33,82 @@ public class CarritoComprasSauce {
 		// driver.manage().window().maximize();
 		// Puedes agregar Firefox u otro navegador aquí
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		System.out.println("Configurgacion finalizada");
 	}
 
 	@Test
 	public void loginTest() throws InterruptedException {
-		Reporter.log("=== INICIANDO TEST DE CARRITO ===",true);
-		driver.get("https://www.saucedemo.com/");
-		Reporter.log("URL cargada: " + driver.getCurrentUrl(),true);
-
-		// Paso 1: Login
-
-		// Verificar que estamos en la página de inventario
-
-		// Paso 2: Agregar productos
-
-		// Verificar el contador del carrito
-
-		// Paso 3: Ir al carrito
-
-		// Paso 4: Checkout
-
-		// Paso 5: Llenar formulario
-
-		// Paso 6: Finalizar compra
-
-		// Paso 7: Verificar confirmación
-
 		
-		Assert.assertTrue(true);
-		Reporter.log("Se ejecuta la prueba correctamente",true);
+		try {
+			
+		
+		//driver.get("https://www.saucedemo.com/");
 
+    	// Paso 1: Login
+          driver.get("https://www.saucedemo.com/");
+          wait.until(ExpectedConditions.titleContains("Swag Labs"));
+          Assert.assertEquals(driver.getTitle(), "Swag Labs");
+          System.out.println("Pagina de Login");
+
+          wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name"))).sendKeys("standard_user");
+          driver.findElement(By.id("password")).sendKeys("secret_sauce");
+          driver.findElement(By.id("login-button")).click();
+
+          // Verificar que estamos en la página de inventario
+          wait.until(ExpectedConditions.urlContains("inventory"));
+          Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
+          System.out.println("Página de inventario");
+
+          // Paso 2: Agregar productos
+          wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-sauce-labs-backpack"))).click();
+          driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
+          System.out.println("Se agrega un producto");
+
+          // Verificar el contador del carrito
+          WebElement badge = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("shopping_cart_badge")));
+          Assert.assertEquals(badge.getText(), "2");
+
+          // Paso 3: Ir al carrito
+          driver.findElement(By.className("shopping_cart_link")).click();
+          wait.until(ExpectedConditions.urlContains("cart"));
+          List<WebElement> items = driver.findElements(By.className("cart_item"));
+          Assert.assertEquals(items.size(), 2);
+          System.out.println("Ir al carrito de compras");
+
+          // Paso 4: Checkout
+          driver.findElement(By.id("checkout")).click();
+          wait.until(ExpectedConditions.urlContains("checkout-step-one"));
+          System.out.println("Checkout");
+
+          // Paso 5: Llenar formulario
+          wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("first-name"))).sendKeys("Javier");
+          driver.findElement(By.id("last-name")).sendKeys("Antunez");
+          driver.findElement(By.id("postal-code")).sendKeys("12345");
+          driver.findElement(By.id("continue")).click();
+          System.out.println("Llena el formulario");
+
+          wait.until(ExpectedConditions.urlContains("checkout-step-two"));
+
+          // Paso 6: Finalizar compra
+          wait.until(ExpectedConditions.elementToBeClickable(By.id("finish"))).click();
+          wait.until(ExpectedConditions.urlContains("checkout-complete"));
+          System.out.println("Finaliza compras");
+
+          // Paso 7: Verificar confirmación
+          WebElement mensaje = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("complete-header")));
+          Assert.assertEquals(mensaje.getText().trim(), "Thank you for your order!");
+          System.out.println("Verifica confirmacion");
+		}catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			Assert.fail(e.getMessage());
+		}
 	}
 
 	@AfterClass
 	public void teardown() {
 		if (driver != null) {
 			driver.quit();
+			Reporter.log("Cierra navegador",true);
 		}
 	}
 }
